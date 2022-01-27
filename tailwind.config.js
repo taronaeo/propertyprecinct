3fc66ec55788d478159916b19f0648a81d54adc8
Aaron Teo <aaron.teo@riv-alumni.com>
2022-01-27 22:36:25 +0800

    chore: add tailwindcss aspect-ratio plugin WARNING: current screen `xs` might change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'media',
   theme: {
+    screens: {
+      xs: '320px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
