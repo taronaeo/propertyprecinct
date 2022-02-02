bb5d20ff61cee36305f97153a78711551fd3833a
Aaron Teo <aaron.teo@riv-alumni.com>
2022-02-02 17:41:23 +0800

    feat: add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,61 @@
+import Button from './components/Button';
 import PictorialUploadCard from './components/PictorialUploadCard';
 
 const App: React.FC = () => {
   return (
-    <div className='p-4 grid gap-4 xs:grid-cols-2'>
-      <h1 className='text-4xl font-extrabold col-span-2'>PropertyPrecinct</h1>
+    <div className='p-4'>
+      <h1 className='mb-4 text-4xl font-extrabold'>PropertyPrecinct</h1>
 
-      <PictorialUploadCard
-        id='test1'
-        label='Overview - Front of Shop (w/ LQ)'
-        importance='required'
-      />
-      <PictorialUploadCard
-        id='test2'
-        label='Overview - Front of Shop (w/o LQ)'
-        importance='required'
-      />
-      <PictorialUploadCard
-        id='test3'
-        label='Corridor (Left View)'
-        importance='optional'
-      />
-      <PictorialUploadCard
-        id='test4'
-        label='Corridor (Right View)'
-        importance='optional'
-      />
-      <PictorialUploadCard
-        id='test5'
-        label='ODA (Wide View)'
-        importance='optional'
-      />
-      <PictorialUploadCard
-        id='test6'
-        label='ODA (Left View)'
-        importance='optional'
-      />
-      <PictorialUploadCard
-        id='test7'
-        label='ODA (Right View)'
-        importance='optional'
-      />
-      <PictorialUploadCard
-        id='test8'
-        label='Trading Area (Photo 1)'
-        importance='optional'
-      />
+      <div className='mb-4 grid gap-4 grid-cols-2'>
+        <PictorialUploadCard
+          id='test1'
+          label='Overview - Front of Shop (w/ LQ)'
+          importance='required'
+        />
+        <PictorialUploadCard
+          id='test2'
+          label='Overview - Front of Shop (w/o LQ)'
+          importance='required'
+        />
+        <PictorialUploadCard
+          id='test3'
+          label='Corridor (Left View)'
+          importance='optional'
+        />
+        <PictorialUploadCard
+          id='test4'
+          label='Corridor (Right View)'
+          importance='optional'
+        />
+        <PictorialUploadCard
+          id='test6'
+          label='ODA (Left View)'
+          importance='optional'
+        />
+        <PictorialUploadCard
+          id='test7'
+          label='ODA (Right View)'
+          importance='optional'
+        />
+        <PictorialUploadCard
+          id='test8'
+          label='Trading Area (Photo 1)'
+          importance='optional'
+        />
 
-      <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
+        {/* <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
       <div className='bg-red-300 rounded-xl xs:h-auto aspect-1'></div>
       <div className='bg-green-300 rounded-xl xs:h-auto aspect-1'></div>
       <div className='bg-purple-300 rounded-xl xs:h-auto aspect-1'></div>
-      <div className='bg-orange-300 rounded-xl xs:h-auto aspect-1'></div>
+      <div className='bg-orange-300 rounded-xl xs:h-auto aspect-1'></div> */}
+      </div>
+
+      <h1 className='mb-4 text-4xl font-bold'>Report Actions</h1>
+
+      <div className='mb-2 grid gap-4 grid-cols-2'>
+        <Button color='green'>Save Report</Button>
+        <Button color='red'>Cancel</Button>
+      </div>
     </div>
   );
 };
---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { SaveIcon } from '@heroicons/react/outline';
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  color: 'green' | 'red';
+  loading?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  color,
+  loading = false,
+  disabled = false,
+  className = '',
+  children,
+  ...props
+}) => {
+  const classNames = [
+    `w-auto aspect-1 rounded-xl`,
+    `flex flex-col align-middle items-center justify-center`,
+    color === 'red' ? 'bg-red-500' : 'bg-green-500',
+    color === 'red' ? 'hover:bg-red-600' : 'hover:bg-green-600',
+  ].join(' ');
+
+  return (
+    <button
+      {...props}
+      disabled={disabled || loading}
+      className={`${classNames} ${className}`}>
+      <SaveIcon className='w-10 h-10 text-gray-300' />
+      <h1 className='font-medium text-xl text-white'>{children}</h1>
+    </button>
+  );
+};
+
+export default Button;
