0f03f03ad09882f422ed9ddeb47c6bcd155bccc6
Aaron Teo <aaron.teo@riv-alumni.com>
2022-02-04 09:37:49 +0800

    temporary push

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,164 @@
 import Button from './components/Button';
-import PictorialUploadCard from './components/PictorialUploadCard';
+import PictorialUploadCard, {
+  PictorialUploadCardProps,
+} from './components/PictorialUploadCard';
+
+type CardProps = Pick<PictorialUploadCardProps, 'id' | 'label' | 'importance'>;
+interface Layout {
+  overviews: CardProps[];
+  signboards: CardProps[];
+  corridors: CardProps[];
+  odas: CardProps[];
+  tradingarea: CardProps[];
+  tradingrooms: CardProps[];
+  tradingtoilets: CardProps[];
+  tradingareadoors: CardProps[];
+  staircases: CardProps[];
+  lqs: CardProps[];
+  aircons: CardProps[];
+  remarks: CardProps[];
+  extras: CardProps[];
+}
 
 const App: React.FC = () => {
+  const layout: Layout = {
+    overviews: [
+      {
+        id: 'w-lq',
+        label: 'Overview of Shop Front (w/ LQ)',
+        importance: 'required',
+      },
+      {
+        id: 'wo-lq',
+        label: 'Overview of Shop Front (w/o LQ)',
+        importance: 'required',
+      },
+      {
+        id: 'rear-lt',
+        label: 'Overview of Shop Rear (Left View)',
+        importance: 'required',
+      },
+      {
+        id: 'rear-rt',
+        label: 'Overview of Shop Rear (Right View)',
+        importance: 'required',
+      },
+    ],
+    signboards: [
+      { id: '1', label: 'Store Signboard', importance: 'required' },
+      { id: '2', label: 'Store Signboard' },
+    ],
+    corridors: [
+      {
+        id: 'lt',
+        label: 'Common Corridor (Left View)',
+        importance: 'required',
+      },
+      {
+        id: 'rt',
+        label: 'Common Corridor (Right View)',
+        importance: 'required',
+      },
+    ],
+    odas: [
+      { id: 'lt', label: 'ODA (Left View)', importance: 'required' },
+      { id: 'rt', label: 'ODA (Right View)', importance: 'required' },
+    ],
+    tradingarea: [
+      { id: '1', label: 'Trading Area', importance: 'required' },
+      { id: '2', label: 'Trading Area' },
+    ],
+    tradingrooms: [
+      { id: '1', label: 'Trading Area Room' },
+      { id: '2', label: 'Trading Area Room' },
+    ],
+    tradingtoilets: [
+      { id: '1', label: 'Trading Area Toilet' },
+      { id: '2', label: 'Trading Area Toilet' },
+    ],
+    tradingareadoors: [
+      { id: '1', label: 'Trading Area Door' },
+      { id: '2', label: 'Trading Area Door' },
+    ],
+    staircases: [{ id: '1', label: 'Staircase' }],
+    lqs: [{ id: '1', label: 'Living Quarters' }],
+    aircons: [],
+    remarks: [],
+    extras: [],
+  };
+
   return (
     <div className='p-4'>
       <h1 className='mb-4 text-4xl font-extrabold'>PropertyPrecinct</h1>
 
       <div className='mb-4 grid gap-4 grid-cols-2'>
-        <PictorialUploadCard
-          id='test1'
-          label='Overview - Front of Shop (w/ LQ)'
-          importance='required'
-        />
-        <PictorialUploadCard
-          id='test2'
-          label='Overview - Front of Shop (w/o LQ)'
-          importance='required'
-        />
-        <PictorialUploadCard id='test3' label='Corridor (Left View)' />
-        <PictorialUploadCard id='test4' label='Corridor (Right View)' />
-        <PictorialUploadCard id='test6' label='ODA (Left View)' />
-        <PictorialUploadCard id='test7' label='ODA (Right View)' />
-        <PictorialUploadCard id='test8' label='Trading Area (Photo 1)' />
+        {layout.overviews.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.signboards.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.corridors.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.odas.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.tradingarea.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.tradingrooms.map(({ ...props }) => (
+          <PictorialUploadCard
+            {...props}
+            key={props.id}
+            label={`${props.label} ${props.id}`}
+          />
+        ))}
+
+        {layout.tradingtoilets.map(({ ...props }) => (
+          <PictorialUploadCard
+            {...props}
+            key={props.id}
+            label={`${props.label} ${props.id}`}
+          />
+        ))}
+
+        {layout.tradingareadoors.map(({ ...props }) => (
+          <PictorialUploadCard
+            {...props}
+            key={props.id}
+            label={`${props.label} ${props.id}`}
+          />
+        ))}
+
+        {layout.staircases.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.lqs.map(({ ...props }) => (
+          <PictorialUploadCard
+            {...props}
+            key={props.id}
+            label={`${props.label} ${props.id}`}
+          />
+        ))}
+
+        {layout.aircons.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.remarks.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
+
+        {layout.extras.map(({ ...props }) => (
+          <PictorialUploadCard {...props} key={props.id} />
+        ))}
 
         {/* <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
       <div className='bg-red-300 rounded-xl xs:h-auto aspect-1'></div>
