46758066dd25659e6fd9228943138b6d827428d0
Aaron Teo <aaron.teo@riv-alumni.com>
2022-02-15 22:37:49 +0800

    refactor: rename recurse function and add `useCallback` hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,23 @@
-import { ReactFragment } from 'react';
-import { Categories, LinearCategories } from './types/Categories';
+import { ReactFragment, useCallback } from 'react';
+
+import { LinearCategories } from './types/Categories';
+import templateFTT from './templates/ftt';
 
 import Button from './components/Button';
 import PictorialUploadCard from './components/PictorialUploadCard';
 
-type Items = [string, LinearCategories];
+type Iteration = [string, LinearCategories];
 
 const App: React.FC = () => {
-  const categories: Categories = {
-    overviews: [
-      {
-        label: 'Overview of Shop Front (w/ LQ)',
-        importance: 'required',
-      },
-      {
-        label: 'Overview of Shop Front (w/o LQ)',
-        importance: 'required',
-      },
-      {
-        label: 'Overview of Shop Rear (Left View)',
-        importance: 'required',
-      },
-      {
-        label: 'Overview of Shop Rear (Right View)',
-        importance: 'required',
-      },
-    ],
-    signboards: [
-      { label: 'Store Signboard', importance: 'required' },
-      { label: 'Store Signboard' },
-    ],
-    corridors: [
-      {
-        label: 'Common Corridor (Left View)',
-        importance: 'required',
-      },
-      {
-        label: 'Common Corridor (Right View)',
-        importance: 'required',
-      },
-    ],
-    odas: [
-      { label: 'ODA (Left View)', importance: 'required' },
-      { label: 'ODA (Right View)', importance: 'required' },
-    ],
-    tradingareas: {
-      rooms: [{ label: 'Trading Area Room' }],
-      toilets: [{ label: 'Trading Area Toilet' }],
-      firedoors: [{ label: 'Trading Area Firedoor' }],
-    },
-    staircases: [{ label: 'Staircase' }],
-    lqs: [{ label: 'Living Quarters' }],
-    aircons: [],
-    remarks: [],
-    extras: [],
-  };
-
-  const recurseCategories = ([cat, items]: Items): ReactFragment => {
+  const recurse = useCallback(([category, items]: Iteration): ReactFragment => {
     if (!Array.isArray(items))
-      return Object.entries(items).map(([subcat, iterable]) =>
-        recurseCategories([`${cat}.${subcat}`, iterable])
+      return Object.entries(items).map(([subcategory, iterable]) =>
+        recurse([`${category}.${subcategory}`, iterable])
       );
 
-    return items.map(({ ...props }, count) => {
-      const id = `${cat}.${count + 1}`;
-      const counter = cat === 'overviews' ? '' : count + 1;
+    return items.map(({ counted = true, ...props }, count) => {
+      const id = `${category}.${count + 1}`;
+      const counter = counted ? count + 1 : '';
 
       return (
         <PictorialUploadCard
@@ -75,14 +28,14 @@ const App: React.FC = () => {
         />
       );
     });
-  };
+  }, []);
 
   return (
     <div className='p-4'>
       <h1 className='mb-4 text-4xl font-extrabold'>PropertyPrecinct</h1>
 
       <div className='mb-4 grid gap-4 grid-cols-2'>
-        {Object.entries(categories).map(recurseCategories)}
+        {Object.entries(templateFTT).map(recurse)}
 
         {/* <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
       <div className='bg-red-300 rounded-xl xs:h-auto aspect-1'></div>
