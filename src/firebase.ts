3b36edbc0cff4b3b80cf239077d0cf27f45491e4
Aaron Teo <aaron.teo@riv-alumni.com>
2022-02-19 20:24:19 +0800

    refactor: add random filename, switch to string upload instead of blob

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,15 @@
+import { nanoid } from 'nanoid';
+
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getDatabase, ref as databaseRef, push } from 'firebase/database';
-import { getStorage, ref as storageRef, uploadBytes } from 'firebase/storage';
+import {
+  getStorage,
+  ref as storageRef,
+  uploadString,
+  getDownloadURL,
+  StorageReference,
+} from 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyC5zbz5tUoTcUHZbzZs4e5k31bHA4D_Q0Y',
@@ -18,6 +26,7 @@ const app = initializeApp(config);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
 const storage = getStorage(app);
+const storageTemp = getStorage(app, 'gs://propertyprecinct-temp');
 
 const databaseApi = {
   reportsRef: 'reports',
@@ -28,10 +37,12 @@ const databaseApi = {
 };
 
 const storageApi = {
-  tempFolder: 'temp',
-  writePreviewToTemp: function (filename: string, preview: Blob) {
-    const fileRef = storageRef(storage, `${this.tempFolder}/${filename}`);
-    return uploadBytes(fileRef, preview);
+  getPreviewUrl: function (ref: StorageReference) {
+    return getDownloadURL(ref);
+  },
+  writePreviewToTemp: function (preview: string) {
+    const fileRef = storageRef(storageTemp, nanoid());
+    return uploadString(fileRef, preview, 'data_url');
   },
 };
 
