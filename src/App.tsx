605fae1472013eb51f33f711f39f3ba043c4d094
Aaron Teo <aaron.teo@riv-alumni.com>
2022-02-19 20:33:30 +0800

    refactor: move report form into a folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
 import { useForm, FormProvider } from 'react-hook-form';
-import { useCallback, ReactFragment } from 'react';
 
-import { LinearCategories } from './types/Categories';
-import fttTemplate from './templates/ftt';
-
-import Button from './components/Button';
-import PictorialUploadCard from './components/PictorialUploadCard';
-
-type Iteration = [string, LinearCategories];
+import UploadReportForm from './forms/UploadReport';
 
 const App: React.FC = () => {
   const methods = useForm();
-  const onSubmit = useCallback((data) => console.log(data), []);
-
-  const recurse = useCallback(([category, items]: Iteration): ReactFragment => {
-    if (!Array.isArray(items))
-      return Object.entries(items).map(([subcategory, iterable]) =>
-        recurse([`${category}.${subcategory}`, iterable])
-      );
-
-    return items.map(({ counted = true, ...props }, count) => {
-      const id = `${category}.${count}`;
-      const counter = counted ? count + 1 : '';
-
-      return (
-        <PictorialUploadCard
-          {...props}
-          key={id}
-          id={id}
-          label={`${props.label} ${counter}`.trimEnd()}
-        />
-      );
-    });
-  }, []);
 
   return (
     <div className='p-4'>
       <h1 className='mb-4 text-4xl font-extrabold'>PropertyPrecinct</h1>
 
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <div className='mb-4 grid gap-4 grid-cols-2'>
-            {Object.entries(fttTemplate).map(recurse)}
-
-            {/* <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
-              <div className='bg-red-300 rounded-xl xs:h-auto aspect-1'></div>
-              <div className='bg-green-300 rounded-xl xs:h-auto aspect-1'></div>
-              <div className='bg-purple-300 rounded-xl xs:h-auto aspect-1'></div>
-              <div className='bg-orange-300 rounded-xl xs:h-auto aspect-1'></div> */}
-          </div>
-
-          <h1 className='mb-4 text-4xl font-bold'>Report Actions</h1>
-
-          <div className='mb-2 grid gap-4 grid-cols-2'>
-            <Button color='green' type='submit'>
-              Save Report
-            </Button>
-
-            <Button color='red' type='reset'>
-              Reset Report
-            </Button>
-          </div>
-        </form>
+        <UploadReportForm />
       </FormProvider>
     </div>
   );
---
+++ src/forms/UploadReport.tsx
@@ -0,0 +1,64 @@
+import { useFormContext } from 'react-hook-form';
+import { useCallback, ReactFragment } from 'react';
+
+import fttTemplate from '../templates/ftt';
+import { LinearCategories } from '../types/Categories';
+
+import Button from '../components/Button';
+import PictorialUploadCard from '../components/PictorialUploadCard';
+
+type Iteration = [string, LinearCategories];
+
+const UploadReport: React.FC = () => {
+  const { handleSubmit } = useFormContext();
+  const onSubmit = useCallback((data) => console.log(data), []);
+
+  const recurse = useCallback(([category, items]: Iteration): ReactFragment => {
+    if (!Array.isArray(items))
+      return Object.entries(items).map(([subcategory, iterable]) =>
+        recurse([`${category}.${subcategory}`, iterable])
+      );
+
+    return items.map(({ counted = true, ...props }, count) => {
+      const id = `${category}.${count}`;
+      const counter = counted ? count + 1 : '';
+
+      return (
+        <PictorialUploadCard
+          {...props}
+          key={id}
+          id={id}
+          label={`${props.label} ${counter}`.trimEnd()}
+        />
+      );
+    });
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='mb-4 grid gap-4 grid-cols-2'>
+        {Object.entries(fttTemplate).map(recurse)}
+
+        {/* <div className='bg-yellow-300 rounded-xl xs:h-auto aspect-1'></div>
+            <div className='bg-red-300 rounded-xl xs:h-auto aspect-1'></div>
+            <div className='bg-green-300 rounded-xl xs:h-auto aspect-1'></div>
+            <div className='bg-purple-300 rounded-xl xs:h-auto aspect-1'></div>
+            <div className='bg-orange-300 rounded-xl xs:h-auto aspect-1'></div> */}
+      </div>
+
+      <h1 className='mb-4 text-4xl font-bold'>Report Actions</h1>
+
+      <div className='mb-2 grid gap-4 grid-cols-2'>
+        <Button color='green' type='submit'>
+          Save Report
+        </Button>
+
+        <Button color='red' type='reset'>
+          Reset Report
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default UploadReport;
